fce369ef5749be1cf7c7913d329783f278ff9c91
YangWenLong123 <496192903@qq.com>
2019-03-22 20:39:18 +0800

    :ambulance:

--- blog/Scaffolding-pc/src/components/HomePageChildren/Whispering.vue
+++ blog/Scaffolding-pc/src/components/HomePageChildren/Whispering.vue
@@ -8,16 +8,20 @@
 				start-placeholder="开始日期", 
 				end-placeholder="结束日期", 
 				change='changeIpt',
-				@change="change",
+				@change="change"
 				value-format=" yyyy-MM-dd")
 			div.al_btn(ref='alBtn', @click='serchSenten()') 搜索
 			el-select(v-model="value2" filterable placeholder="请选择")
 				el-option(v-for="item in options", :key="item.value", :label="item.label", :value="item.value")
 			div.al_btn2(ref='alBtn2',@click='serchSenten2()') 搜索
 		el-main
-			p 内容
+			div.al_txt(v-for='(item,index) in data', :data='item.category')
+				div.al_txtCont {{item.id}}. {{item.cont}}
+				div.al_desc 
+					p.J_al_descTime 发表时间：{{item.time}}
+					p.J_al_sescCate 分类:{{item.category}}
 		el-footer
-			el-pagination(:page-size="10", :pager-count="11", layout="prev, pager, next", :total="500", @current-change="handleCurrentChange")
+			el-pagination(:page-size="10", :pager-count="11", layout="prev, pager, next", :total="100", @current-change="handleCurrentChange")
 </template>
 
 <script>
@@ -27,47 +31,49 @@ export default {
 			return{
 				value: '',
 				value2: '',
-				options: [{
-          value: '励志',
-          label: '励志'
-        }, {
-          value: '青春',
-          label: '青春'
-        }, {
-          value: '搞笑',
-          label: '搞笑'
-        }, {
-          value: '情感',
-          label: '情感'
-        }, {
-          value: '幽默',
-          label: '幽默'
-        }],
+				options: [
+					{value: '励志',label: '励志'}, 
+					{value: '青春',label: '青春'}, 
+					{value: '搞笑',label: '搞笑'}, 
+					{value: '情感',label: '情感'}, 
+					{value: '幽默',label: '幽默'}
+				],
+				data: '',
+				newdata: ''
 			}
 		},
 		created () {
 			this.$http.get("/wy").then(res => {
-				console.log(res)
+				let data = res.data.data
+				let arr = []; // 创建数组分割数
+				for (var i = 0; i < data.length; i += 5) { // 对数据进行分割处理
+					arr.push(data.slice(i, i + 5));
+				}
+				this.data = arr[0];
+				this.newdata = arr;
+
 			});
 		},
 		mounted () {
+			setTimeout(()=>{
 			
+			},200)
 		},
 		methods: {
 			change(){	//选择日期时默认执行
 			},
-			serchSenten () {	//点击搜索
+			serchSenten () {	//日期范围搜索
 				let startTime = new Date(this.value[0]).getTime(),
 						endTime   = new Date(this.value[0]).getTime();
 				if(!isNaN(startTime) && !isNaN(endTime)){
 					console.log(startTime,endTime)
 				}
 			},
-			serchSenten2 () {
+			serchSenten2 () {	//模糊搜索
 				alert(this.value2)
 			},
-			handleCurrentChange (val) {
-				console.log(val)
+			handleCurrentChange (val) {	//分页
+				this.data = this.newdata[val-1]
 			}
 		},
 		components: {
@@ -80,7 +86,7 @@ export default {
 	.al_box{
 		font-family: "Helvetica Neue",Helvetica,"PingFang SC","Hiragino Sans GB","Microsoft YaHei","微软雅黑",Arial,sans-serif;
 		width: 1200px;
-		height: 700px;
+		// height: 700px;
 		overflow: hidden;
 		margin: 20px auto;
 		background: rgba(0, 0, 0, 0.2);
@@ -90,13 +96,60 @@ export default {
 			height: 40px;
 			overflow: hidden;
 			border-radius: 5px;
-			background: rgba(0,0,0,.5);
+			// background: rgba(0,0,0,.5);
 			text-align: center;
 			line-height: 40px;
 			font-size: 16px;
 			margin-left: 30px;
 			border: 1px @b solid;
 			color: #DCDFE6;
+			margin-right: 130px;
+		}
+		.al_txt{
+			width: 1140px;
+			overflow: hidden;
+			color: @b;
+			background: rgba(0,0,0,.5);
+			border-radius: 20px;
+			margin-bottom: 20px;
+			// border: 1px @b solid;
+			.al_txtCont{
+				width: 1100px;
+				height: auto;
+				overflow: hidden;
+				line-height: 23px;
+				font-size: 16px;
+				color: #DCDFE6;
+				margin: 0 auto;
+				margin-top: 10px;
+				&:hover{
+					color: salmon;
+				}
+			}
+			.al_desc{
+				width: 1100px;
+				height: 30px;
+				overflow: hidden;
+				margin: 0 auto;
+				// border: 1px red solid;
+				display: flex;
+				align-items: center;
+				.J_al_descTime{
+					margin-left: 880px;
+					&:hover{
+					color: salmon;
+				}
+				}
+				.J_al_sescCate{
+					margin-left: 30px;
+					&:hover{
+					color: salmon;
+				}
+				}
+			}
+		}
+		.al_txt:first-child{
+			margin-top: 10px;
 		}
 	}
 </style>
